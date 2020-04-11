28f125740bed02cc26ed6d7384ab4d00901577ed
RAJKUMAR0003 <63493838+RAJKUMAR0003@users.noreply.github.com>
2020-04-11 14:13:26 +0530

    Update perfect_square.py

--- perfect_square.py
+++ perfect_square.py
@@ -1,5 +1,5 @@
 from math import sqrt,floor
-n=int(input('enter a no upto whuch you wanna print the perfect square no '))
+n=int(input('enter a no upto which you wanna print the perfect square no '))
 for i in range(1,n+1):
     a= sqrt(int(i))
     if(a-floor(a)==0):
